357ec7033d23136f97065dd08c72c6c8758f5435
AlekVolsk <alekvolsk@yandex.ru>
2019-08-19 03:22:41 +0400

    v1.0.2

--- plg_content_vmvideo/assets/vmvideo.js
+++ plg_content_vmvideo/assets/vmvideo.js
@@ -4,9 +4,24 @@
  * @copyright   Copyright (C) 2019 Aleksey A. Morozov. All rights reserved.
  * @license     GNU General Public License version 3 or later; see http://www.gnu.org/licenses/gpl-3.0.txt
  */
+
+(function () {
+    if (!Array.prototype.forEach) {
+        Array.prototype.forEach = function (callback, thisArg) {
+            thisArg = thisArg || window;
+            for (var i = 0; i < this.length; i++) {
+                callback.call(thisArg, this[i], i, this);
+            }
+        };
+    }
+    if (typeof NodeList.prototype.forEach !== "function") {
+        NodeList.prototype.forEach = Array.prototype.forEach;
+    }
+})();
+
 document.addEventListener('DOMContentLoaded', function () {
-    if (document.querySelector('.vmvideo > .vmvideo-cover')) {
-        document.querySelector('.vmvideo > .vmvideo-cover').addEventListener('click', function (e) {
+    document.querySelectorAll('.vmvideo > .vmvideo-cover').forEach(function (el) {
+        el.addEventListener('click', function (e) {
             var target = e.target || e.srcElement;
             var params = JSON.parse(target.dataset.videoparams.replace(/\'/g, '"'));
             var query = [];
@@ -15,5 +30,5 @@ document.addEventListener('DOMContentLoaded', function () {
             }
             target.innerHTML = '<embed src="//player.vimeo.com/video/' + target.dataset.videosrc + '?' + query.join('&') + '" allow="accelerometer;autoplay;encrypted-media;gyroscope;picture-in-picture" allowfullscreen style="border:0;"></embed>';
         });
-    }
+    });
 });
